f0413d03e7f56d363f1e708f3faeb28573028087
rushab14 <rushabpk31@gmail.com>
2023-12-15 14:05:40 +0530

    refactor

--- PL/ApartmentApp/src/app/app-routing.module.ts
+++ PL/ApartmentApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FacilityMgtComponent } from './facility-mgt/facility-mgt.component';
 import { SigninComponent } from './sign-in/sign-in.component';
+import { OwnerComponent } from './owner/owner.component';
 
 const routes: Routes = [
   {path:'getOwner',component:FacilityMgtComponent},
@@ -9,7 +10,8 @@ const routes: Routes = [
    {path:'BookFacilty',component:FacilityMgtComponent},
    {path:'SearchByFName',component:FacilityMgtComponent},
    {path:'SignIn',component:SigninComponent},
-   {path:'freeFacility',component:FacilityMgtComponent}
+   {path:'freeFacility',component:FacilityMgtComponent},
+   {path:'createOwner',component:OwnerComponent}
    
 ];
 
--- PL/ApartmentApp/src/app/owner/owner.component.ts
+++ PL/ApartmentApp/src/app/owner/owner.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-owner',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./owner.component.css']
 })
 export class OwnerComponent {
+  id:number=0;
+  name: string='';
+  phone: string='';
 
+  
+  constructor(private http: HttpClient,private router:Router) {}
+
+  onSubmit() {
+    const url = 'https://localhost:7003/api/createowner';
+    const data = { id:this.id,name: this.name, phone: this.phone };
+
+    this.http.post(url, data).subscribe(response => {
+      console.log(response);
+    
+    });
+    this.router.navigate(['/SignIn'])
+    
+  }
 }
+
+
+
+
+
